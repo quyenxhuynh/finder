af5856a67ba68dd06c49daf9c989e2d98490ea9d
Quyen Huynh <quyenhuynh@Quyens-MacBook-Pro.local>
2020-11-06 04:06:47 -0500

    test

--- finder/settings.py
+++ finder/settings.py
@@ -108,12 +108,6 @@ WSGI_APPLICATION = 'finder.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': str(BASE_DIR / 'db.sqlite3'),
-#     }
-# }
 
 DATABASES = {
     'default': {
