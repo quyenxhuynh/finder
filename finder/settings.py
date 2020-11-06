e87d9ee72035f2c9659ca0607d3aa9ad03078b20
Quyen Huynh <quyenhuynh@Quyens-MacBook-Pro.local>
2020-11-06 01:41:55 -0500

    test

--- finder/settings.py
+++ finder/settings.py
@@ -126,7 +126,12 @@ DATABASES = {
     }
 }
 
-GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
+
+GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
 
 
 
